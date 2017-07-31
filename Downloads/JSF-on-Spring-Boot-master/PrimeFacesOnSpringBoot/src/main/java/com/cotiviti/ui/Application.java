0497feffae9b96e40ff98984e3372c18b6e42c96
Tony Morris <anthony.morris@appsassociates.com>
2017-07-31 04:21:10 -0400

    initial

---
+++ Downloads/JSF-on-Spring-Boot-master/PrimeFacesOnSpringBoot/src/main/java/com/cotiviti/ui/Application.java
@@ -0,0 +1,55 @@
+package com.cotiviti.ui;
+
+import javax.faces.application.ProjectStage;
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.primefaces.util.Constants;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.request.RequestContextListener;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+    
+    @Override 
+	 public void onStartup( ServletContext servletContext ) throws ServletException {
+	        super.onStartup( servletContext );
+	        servletContext.addListener( new RequestContextListener() ) ;
+	    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        FacesServlet servlet = new FacesServlet();
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet, "*.xhtml");
+		return servletRegistrationBean;
+    }
+    
+    @Bean
+	public ServletContextInitializer servletContextCustomizer() {
+	    return new ServletContextInitializer() {
+            @Override
+            public void onStartup(ServletContext sc) throws ServletException {
+                sc.setInitParameter(Constants.ContextParams.THEME, "glass-x");
+                sc.setInitParameter(Constants.ContextParams.FONT_AWESOME, "true");
+                sc.setInitParameter(ProjectStage.PROJECT_STAGE_PARAM_NAME, ProjectStage.Development.name());
+            }
+	    };
+	}
+    
+}
