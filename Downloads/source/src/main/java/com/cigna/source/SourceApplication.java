f6364ea84cec880a88a093ae2ebb95bd979cdf08
Tony Morris <anthony.morris@appsassociates.com>
2019-01-13 12:27:51 -0500

    Change to activemq

--- Downloads/source/src/main/java/com/cigna/source/SourceApplication.java
+++ Downloads/source/src/main/java/com/cigna/source/SourceApplication.java
@@ -1,55 +1,13 @@
 package com.cigna.source;
 
-import java.util.Date;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.context.annotation.Bean;
-import org.springframework.integration.annotation.InboundChannelAdapter;
-import org.springframework.integration.core.MessageSource;
-import org.springframework.integration.support.MessageBuilder;
-import org.springframework.integration.annotation.Poller;
-
-
 
 @SpringBootApplication
-@EnableBinding(Source.class)
 public class SourceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SourceApplication.class, args);
 	}
-	
-	@Bean
-	@InboundChannelAdapter(value = Source.OUTPUT, poller = @Poller(fixedDelay = "10000", maxMessagesPerPoll = "1"))
-	public MessageSource<TimeInfo> timerMessageSource() {
-		return () -> MessageBuilder.withPayload(new TimeInfo(new Date().getTime()+"","Label")).build();
-	}
- 
-	public static class TimeInfo{
- 
-		private String time;
-		private String label;
- 
-		public TimeInfo(String time, String label) {
-			super();
-			this.time = time;
-			this.label = label;
-		}
- 
-		public String getTime() {
-			return time;
-		}
- 
-		public String getLabel() {
-			return label;
-		}
- 
-	}
- 
+
 }
-
-
-
